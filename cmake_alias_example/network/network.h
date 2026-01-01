3fbc4691546dd2a26f0bdedca32094bd4344f694
tangyz <tyz198187@163.com>
2026-01-01 19:49:06 +0800

    增加项目

---
+++ cmake_alias_example/network/network.h
@@ -0,0 +1,13 @@
+﻿// network.cpp - 网络功能演示
+#include <iostream>
+#include <string>
+
+#ifdef HAVE_OPENSSL
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <openssl/rand.h>
+#endif
+
+void demonstrate_openssl_usage();
+
+void demonstrate_network_capabilities();
