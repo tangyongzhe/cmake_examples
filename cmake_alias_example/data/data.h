29ddbb9ea94289f4fc5c6568627aa5e0b9814c29
tangyz <tyz198187@163.com>
2026-01-04 10:21:06 +0800

    Squashed 'transmit_asio/' content from commit 8f42585
    git-subtree-dir: transmit_asio
    git-subtree-split: 8f42585d18ba0e8a29cbcd39cefe6b9bf8f88ddb

---
+++ cmake_alias_example/data/data.h
@@ -0,0 +1,12 @@
+﻿// data.cpp - 数据处理演示
+#include <iostream>
+#include <string>
+#include <vector>
+
+#ifdef HAVE_ZLIB
+#include <zlib.h>
+#endif
+
+void demonstrate_compression_capabilities();
+
+void demonstrate_data_processing_features();
